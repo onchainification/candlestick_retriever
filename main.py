fcffcd87be2e36d1c8fc9aa6038908a115a42bf5
Jorijn Jacko Smit <github@jorijnsmit.com>
2020-02-15 13:51:42 +0100

    continue from previous saved data, concat instead of append api results

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import requests
 from datetime import datetime
 
@@ -12,70 +13,51 @@ def get_batch(symbol, interval, start_time=0, limit=1000):
         'startTime': start_time,
         'limit': limit
     }
-    response = requests.get('https://api.binance.com/api/v3/klines', params)
-    return response.json()
+    try:
+        response = requests.get('https://api.binance.com/api/v3/klines', params)
+    except requests.exceptions.ConnectionError:
+        print('Cooling down for 5 mins...')
+        time.sleep(5 * 60)
+        return get_batch(symbol, interval, start_time, limit)
+    return pd.DataFrame(response.json())
 
 
-def get_all_candles(symbol='BTCUSDT', interval='1m'):
+def all_candles_to_csv(base='BTC', quote='USDT', interval='1m'):
     """fill a dataframe with all candlesticks of a trading pair since its inception"""
 
-    df = pd.DataFrame.from_dict(get_batch(symbol=symbol, interval=interval, limit=1))
-    while True:
+    # see if there is any data saved already
+    try:
+        df = pd.read_csv(f'data/{base}-{quote}.csv')
+
+        # column labels from the csv come back as strings vs ints of api response
+        df.columns = df.columns.astype(int)
+
         last_timestamp = df.iloc[-1, 0]
+    except FileNotFoundError:
+        last_timestamp = 0
+
+    last_datetime = datetime.fromtimestamp(last_timestamp / 1000)
+
+    batches = []
+
+    # gather all batches available, starting from the last timestamp saved
+    # stop if candlesticks with date of today are reached
+    while last_datetime.strftime('%Y%m%d') < datetime.now().strftime('%Y%m%d'):
+        batches.append(get_batch(symbol=base+quote, interval=interval, start_time=last_timestamp))
+
+        last_timestamp = batches[-1].iloc[-1, 0]
         last_datetime = datetime.fromtimestamp(last_timestamp / 1000)
-        df = df.append(
-            get_batch(symbol=symbol, interval=interval, start_time=last_timestamp),
-            ignore_index=True)
-        print(symbol, interval, last_datetime)
-        if last_datetime.strftime('%Y%m%d') >= datetime.now().strftime('%Y%m%d'):
-            return df
+
+        print(base, quote, interval, last_datetime)
+
+    if len(batches) > 0:
+        pd.concat(batches, ignore_index=True).to_csv(f'data/{base}-{quote}.csv', index=False)
+        return True
 
 
-def quick_clean(df):
-    """add headers and convert all columns to their proper dtype"""
-
-    df.columns = [
-        'open_time',
-        'open',
-        'high',
-        'low',
-        'close',
-        'volume',
-        'close_time',
-        'quote_asset_volume',
-        'number_of_trades',
-        'taker_buy_base_asset_volume',
-        'taker_buy_quote_asset_volume',
-        'ignore'
-    ]
-
-    df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
-    df = df.set_index('open_time', drop=True)
-
-    df = df.astype(dtype={
-        'open': 'float64',
-        'high': 'float64',
-        'low': 'float64',
-        'close': 'float64',
-        'volume': 'float64',
-        'close_time': 'datetime64[ms]',
-        'quote_asset_volume': 'float64',
-        'number_of_trades': 'int64',
-        'taker_buy_base_asset_volume': 'float64',
-        'taker_buy_quote_asset_volume': 'float64',
-        'ignore': 'float64'
-    })
-
-    return df
-
-
-trading_pairs = []
 for pair in requests.get('https://api.binance.com/api/v3/exchangeInfo').json()['symbols']:
-    trading_pairs.append(pair['symbol'])
-
-trading_pairs = ['ETHUSDT', 'BNBUSDT']
-
-for pair in trading_pairs:
-    df = get_all_candles(symbol=pair)
-    df = quick_clean(df)
-    df.to_csv(f'{pair}.csv')
+    if pair['quoteAsset'] == 'USDT':
+        if all_candles_to_csv(base=pair['baseAsset'], quote=pair['quoteAsset']) is True:
+            print(f'Wrote new candles to file for {pair["symbol"]}')
+        else:
+            print(f'Already up to date with {pair["symbol"]}')
