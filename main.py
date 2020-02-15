cc21b71f55d6e4edc2253e4c5557aa0c815a4b94
Jorijn Jacko Smit <github@jorijnsmit.com>
2020-02-15 21:08:44 +0100

    check requests' code (#1), always label columns (#3), sort csv files

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from datetime import datetime
 
 import pandas as pd
 
+LABELS = [
+    'open_time',
+    'open',
+    'high',
+    'low',
+    'close',
+    'volume',
+    'close_time',
+    'quote_asset_volume',
+    'number_of_trades',
+    'taker_buy_base_asset_volume',
+    'taker_buy_quote_asset_volume',
+    'ignore'
+]
+
 def get_batch(symbol, interval, start_time=0, limit=1000):
     """get as many candlesticks as possible in one go"""
 
@@ -19,26 +34,27 @@ def get_batch(symbol, interval, start_time=0, limit=1000):
         print('Cooling down for 5 mins...')
         time.sleep(5 * 60)
         return get_batch(symbol, interval, start_time, limit)
-    return pd.DataFrame(response.json())
+    if response.status_code == 200:
+        return pd.DataFrame(response.json(), columns=LABELS)
+    print(f'Got crappy response back: {response}')
 
 
 def all_candles_to_csv(base='BTC', quote='USDT', interval='1m'):
-    """fill a dataframe with all candlesticks of a trading pair since its inception"""
+    """
+    collect a list of candlestick batches with all candlesticks of a trading pair,
+    concat into a dataframe and write it to csv
+    """
 
+    batches = []
     # see if there is any data saved already
     try:
-        df = pd.read_csv(f'data/{base}-{quote}.csv')
-
-        # column labels from the csv come back as strings vs ints of api response
-        df.columns = df.columns.astype(int)
-
+        df = pd.read_csv(f'data/{quote}/{base}-{quote}.csv')
         last_timestamp = df.iloc[-1, 0]
     except FileNotFoundError:
         last_timestamp = 0
+        batches.append(pd.DataFrame([], columns=LABELS))
 
     last_datetime = datetime.fromtimestamp(last_timestamp / 1000)
-
-    batches = []
 
     # gather all batches available, starting from the last timestamp saved
     # stop if candlesticks with date of today are reached
@@ -50,14 +66,15 @@ def all_candles_to_csv(base='BTC', quote='USDT', interval='1m'):
 
         print(base, quote, interval, last_datetime, end='\r', flush=True)
 
-    if len(batches) > 0:
-        pd.concat(batches, ignore_index=True).to_csv(f'data/{base}-{quote}.csv', index=False)
+    if len(batches) > 1:
+        df = pd.concat(batches, ignore_index=True)
+        df.to_csv(f'data/{quote}/{base}-{quote}.csv', index=False)
         return True
 
-
-for pair in requests.get('https://api.binance.com/api/v3/exchangeInfo').json()['symbols']:
-    if pair['quoteAsset'] == 'USDT' and pair['status'] == 'TRADING':
-        if all_candles_to_csv(base=pair['baseAsset'], quote=pair['quoteAsset']) is True:
-            print(f'Wrote new candles to file for {pair["symbol"]}')
-        else:
-            print(f'Already up to date with {pair["symbol"]}')
+while True:
+    for pair in requests.get('https://api.binance.com/api/v3/exchangeInfo').json()['symbols']:
+        if pair['quoteAsset'] == 'USDT' and pair['status'] == 'TRADING':
+            if all_candles_to_csv(base=pair['baseAsset'], quote=pair['quoteAsset']) is True:
+                print(f'Wrote new candles to file for {pair["symbol"]}')
+            else:
+                print(f'Already up to date with {pair["symbol"]}')
