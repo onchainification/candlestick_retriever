b18eadce3f42f9f764dd4cd44bf156817b8d7140
Jorijn Jacko Smit <github@jorijnsmit.com>
2020-02-17 17:46:39 +0100

    suboptimal solution for uploading (#2), more efficient date comparisons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
 import time
-from datetime import datetime
+from datetime import date, datetime, timedelta
 
 import requests
 import pandas as pd
@@ -36,8 +37,8 @@ def get_batch(symbol, interval, start_time=0, limit=1000):
         return get_batch(symbol, interval, start_time, limit)
     if response.status_code == 200:
         return pd.DataFrame(response.json(), columns=LABELS)
-    print(f'Got crappy response back: {response}')
-    return False
+    print(f'Got erroneous response back: {response}')
+    return None
 
 
 def all_candles_to_csv(base='BTC', quote='USDT', interval='1m'):
@@ -46,8 +47,8 @@ def all_candles_to_csv(base='BTC', quote='USDT', interval='1m'):
     concat into a dataframe and write it to csv
     """
 
-    batches = []
     # see if there is any data saved already
+    batches = []
     try:
         batches.append(pd.read_csv(f'data/{base}-{quote}.csv'))
         last_timestamp = batches[-1].iloc[-1, 0]
@@ -56,30 +57,43 @@ def all_candles_to_csv(base='BTC', quote='USDT', interval='1m'):
         last_timestamp = 0
 
     # gather all batches available, starting from the last timestamp saved or 0
-    # stop if the timestamp that comes back is the same as the last one saved
+    # stop if the timestamp that comes back from the api is the same as the last one saved
     previous_timestamp = None
-    while previous_timestamp != last_timestamp:
-        previous_timestamp = last_timestamp
+    if date.fromtimestamp(last_timestamp / 1000) < date.today():
+        while previous_timestamp != last_timestamp:
+            previous_timestamp = last_timestamp
 
-        batches.append(get_batch(symbol=base+quote, interval=interval, start_time=last_timestamp))
+            batches.append(get_batch(
+                symbol=base+quote,
+                interval=interval,
+                start_time=last_timestamp
+            ))
 
-        last_timestamp = batches[-1].iloc[-1, 0]
-        last_datetime = datetime.fromtimestamp(last_timestamp / 1000)
+            last_timestamp = batches[-1].iloc[-1, 0]
+            last_datetime = datetime.fromtimestamp(last_timestamp / 1000)
 
-        covering_spaces = 20 * ' '
+            covering_spaces = 20 * ' '
+            print(base, quote, interval, str(last_datetime)+covering_spaces, end='\r', flush=True)
 
-        print(base, quote, interval, last_datetime+covering_spaces, end='\r', flush=True)
-
+    # in the case that new data was gathered write it to a csv file
     if len(batches) > 1:
-        # this means new data was gathered; write it to file
         df = pd.concat(batches, ignore_index=True)
         df.to_csv(f'data/{base}-{quote}.csv', index=False)
         return True
 
-while True:
+if __name__ == '__main__':
+    # do a full update on all currency pairs that have BTC as their quote currency
     for pair in requests.get('https://api.binance.com/api/v3/exchangeInfo').json()['symbols']:
-        if pair['quoteAsset'] == 'USDT' and pair['status'] == 'TRADING':
+        if pair['quoteAsset'] == 'BTC':
             if all_candles_to_csv(base=pair['baseAsset'], quote=pair['quoteAsset']) is True:
                 print(f'Wrote new candles to file for {pair["symbol"]}')
             else:
                 print(f'Already up to date with {pair["symbol"]}')
+
+    # clean the data folder and upload a new version of the dataset to kaggle
+    try:
+        os.remove('data/.DS_Storee')
+    except FileNotFoundError:
+        pass
+    YESTERDAY = date.today() - timedelta(days=1)
+    os.system(f'kaggle datasets version -p data/ -m "full update up till {str(YESTERDAY)}"')
