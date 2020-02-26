fe4c54bf12554c6dead1c583942a81d616060889
Jorijn Jacko Smit <github@jorijnsmit.com>
2020-02-26 07:24:57 +0100

    some functions to help preprocess the data. groom_data() solves #10.

---
+++ preprocessing.py
@@ -0,0 +1,66 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+
+def set_dtypes(df):
+    """
+    set datetimeindex and convert all columns in pd.df to their proper dtype
+    assumes csv is read raw without modifications; pd.read_csv(csv_filename)
+    """
+
+    df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
+    df = df.set_index('open_time', drop=True)
+
+    df = df.astype(dtype={
+        'open': 'float64',
+        'high': 'float64',
+        'low': 'float64',
+        'close': 'float64',
+        'volume': 'float64',
+        'close_time': 'datetime64[ms]',
+        'quote_asset_volume': 'float64',
+        'number_of_trades': 'int64',
+        'taker_buy_base_asset_volume': 'float64',
+        'taker_buy_quote_asset_volume': 'float64',
+        'ignore': 'float64'
+    })
+
+    return df
+
+
+def assert_integrity(df):
+    """make sure no rows have empty cells or duplicate timestamps exist"""
+
+    assert df.isna().all(axis=1).any() == False
+    assert df.index.duplicated().any() == False
+
+
+def quick_clean(df, filename):
+    """clean a raw dataframe"""
+
+    # convert to proper dtypes, drop dupes
+    df = df.set_index('open_time')
+    dupes = df.index.duplicated().sum()
+    if dupes > 0:
+        df = df[df.index.duplicated() == False]
+        print(f'{datetime.now()} Dropped {dupes} duplicates in {filename}')
+
+    # sort by timestamp, oldest first
+    df.sort_values(by=['open_time'], ascending=False)
+
+    # just a doublcheck
+    assert_integrity(df)
+
+    print(f'{datetime.now()} {filename} is clean')
+
+    return df
+
+
+def groom_data(dirname='data'):
+    """go through data folder and perform a quick clean on all csv files"""
+
+    for filename in os.listdir(dirname):
+        if filename.endswith('.csv'):
+            full_path = f'{dirname}/{filename}'
+            quick_clean(pd.read_csv(full_path), filename).to_csv(full_path)
